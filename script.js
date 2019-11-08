707e009171420b32a7afe65b69137ca47b8072c3
Srikar <ssiyer0208@gmail.com>
2019-11-08 20:35:04 +0530

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+const select = document.querySelector("select");
+const result = document.querySelector(".result");
+
+printresult(select.value)
+
+select.addEventListener("change", e => {
+  const selected = select.value;
+
+  printresult(selected);
+});
+
+function printresult(subject) {
+  const html = [
+  	`http://cbseacademic.nic.in/web_material/SQP/ClassXII_2019_20/${subject}_SQP.pdf`,
+    `http://cbseacademic.nic.in/web_material/SQP/ClassXII_2019_20/${subject}_MS.pdf`
+  ].map(link => `<embed src="${link}">`).join('');
+  result.innerHTML = html;
+}
